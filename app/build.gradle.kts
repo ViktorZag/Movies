a2654cfabac003a2633186155286623a0049eb2f
Viktor Zahurskyi <viktorzahurskiy@gmail.com>
2023-10-09 22:04:02 +0300

    Gradle configured & added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,15 +2,20 @@
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.plugin.serialization)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.google.dagger.hilt.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.filmcenter.movies"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.filmcenter.movies"
         minSdk = 26
+        //noinspection OldTargetApi
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -31,23 +36,25 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.7"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/gradle/incremental.annotation.processors"
         }
     }
+    namespace = "com.filmcenter.movies"
 }
 
 dependencies {
@@ -67,4 +74,32 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+    implementation(libs.material3.window.size)
+    //Coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    implementation(libs.coroutines.lifecycle)
+    implementation(libs.coroutines.lifecycle.viewmodel)
+    //Di
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compose)
+    ksp(libs.hilt.compiler)
+    //Room
+    implementation(libs.room.core)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    //Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter)
+    implementation(libs.okhttp)
+    implementation(libs.retrofit.gson)
+    //Serialization
+    implementation(libs.serialization.json)
+    //Datastore
+    implementation(libs.datastore)
+    //Coil
+    implementation(libs.coil)
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,13 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.google.dagger.hilt.android) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.ksp) apply false
+}
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
 true // Needed to make the Suppress annotation work for the plugins block
